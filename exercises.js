556b24e2093a83f951bab82df9ed1b9438b45588
Tony Tezak <anthonyjtezak@gmail.com>
2018-06-12 18:39:53 -1000

    Finished #11

--- exercises.js
+++ exercises.js
@@ -174,8 +174,18 @@ console.log(copyValuesArray);
 Declare a variable named `topQuote` and assign it to a String value of your favorite one line quote.
 Write a function that will iterate through the string value and return the longest word in that quote. Console.log your result.
 */
-
-
+var topQuote = "Look, if we built this large wooden badger ...";
+function longestWord(str){
+  var strToArray = str.split(" ");
+  var currentWord = "";
+  for(i=0; i<strToArray.length; i++){
+    if(strToArray[i].length > currentWord.length){
+      currentWord = strToArray[i]
+    }
+  }
+  return currentWord
+}
+console.log(longestWord(topQuote));
 
 /* 12) Puppet Master
 Declare a variable named `miscStorage` set it's value to be: `[ [], 'Carrots', 9, 'Beets', {}, {name: "Todd B."}, 'Mush' ]`
