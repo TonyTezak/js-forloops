58ada928b9430480d4d9987f22223710ec41aff1
Tony Tezak <anthonyjtezak@gmail.com>
2018-06-12 15:43:45 -1000

    Finished #8

--- exercises.js
+++ exercises.js
@@ -117,8 +117,13 @@ down
 nope
 turn
 */
-
-
+var oopsArray = ['turn', , 'down', , 'for', , 'what' ]
+for(var i=0; i<oopsArray.length; i++){
+  if(i%2 === 1){
+    oopsArray[i] = "nope"
+  }
+}
+console.log(oopsArray.reverse())
 /* 9) Siesta Time
 Declare a variable named `napSchedule` and assign its value to the following array: `[false, false, true, false, true, true]`
 
